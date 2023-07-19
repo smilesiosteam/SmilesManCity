e6572aa19a5e4cbfe66cdcf85e65be0238baebba
Shmeel Ahmed <shmeel.ahmed@systemsltd.com>
2023-07-19 13:24:19 +0500

    welcome flag

--- Sources/SmilesManCity/ViewControllers/ManCityHome/ManCityHomeViewController.swift
+++ Sources/SmilesManCity/ViewControllers/ManCityHome/ManCityHomeViewController.swift
@@ -31,6 +31,7 @@ public class ManCityHomeViewController: UIViewController {
     var sections = [ManCitySectionData]()
     var isUserSubscribed: Bool? = nil
     var aboutVideoUrl: String?
+    var needsWelcome = false
     private var subscriptionInfo: SubscriptionInfoResponse?
     private var userData: RewardPointsResponseModel?
     private var proceedToPayment: ((_ lifeStyleOffer: BOGODetailsResponseLifestyleOffer, _ playerID: String, _ referralCode: String, _ hasAttendedManCityGame:Bool , _ appliedPromoCode: BOGOPromoCode?, _ priceAfterPromo: Double?, _ themeResources: ThemeResources?, _ isComingFromSpecialOffer: Bool, _ isComingFromTreasureChest: Bool) -> Void)?
@@ -67,11 +68,12 @@ public class ManCityHomeViewController: UIViewController {
         }
     }
     
-    public init(categoryId: Int, isUserSubscribed: Bool? = nil, aboutVideoUrl: String? = nil, proceedToPayment: @escaping ((_ lifeStyleOffer: BOGODetailsResponseLifestyleOffer, _ playerID: String, _ referralCode: String, _ hasAttendedManCityGame:Bool , _ appliedPromoCode: BOGOPromoCode?, _ priceAfterPromo: Double?, _ themeResources: ThemeResources?, _ isComingFromSpecialOffer: Bool, _ isComingFromTreasureChest: Bool) -> Void)) {
+    public init(categoryId: Int, isUserSubscribed: Bool? = nil, aboutVideoUrl: String? = nil, needsWelcome:Bool = false, proceedToPayment: @escaping ((_ lifeStyleOffer: BOGODetailsResponseLifestyleOffer, _ playerID: String, _ referralCode: String, _ hasAttendedManCityGame:Bool , _ appliedPromoCode: BOGOPromoCode?, _ priceAfterPromo: Double?, _ themeResources: ThemeResources?, _ isComingFromSpecialOffer: Bool, _ isComingFromTreasureChest: Bool) -> Void)) {
         self.categoryId = categoryId
         self.isUserSubscribed = isUserSubscribed
         self.proceedToPayment = proceedToPayment
         self.aboutVideoUrl = aboutVideoUrl
+        self.needsWelcome = needsWelcome
         super.init(nibName: "ManCityHomeViewController", bundle: Bundle.module)
     }
     
@@ -359,6 +361,9 @@ extension ManCityHomeViewController {
     }
     
     private func configureAboutVideo(with url: String) {
+        if needsWelcome && !url.isEmpty {
+            //TODO: - openPlayer
+        }
         if !url.isEmpty {
             if let aboutVideoIndex = getSectionIndex(for: .about) {
                 let aboutVideo = AboutVideo(videoUrl: url)
