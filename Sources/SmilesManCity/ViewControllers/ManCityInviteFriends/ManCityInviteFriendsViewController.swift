8b99796d6d445b53b9387875091a781674906473
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-07-27 16:10:45 +0500

    - Created invite friends controller

---
+++ Sources/SmilesManCity/ViewControllers/ManCityInviteFriends/ManCityInviteFriendsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ManCityInviteFriendsViewController.swift
+//  
+//
+//  Created by Abdul Rehman Amjad on 27/07/2023.
+//
+
+import UIKit
+
+class ManCityInviteFriendsViewController: UIViewController {
+
+    // MARK: - OUTLETS -
+    
+    
+    // MARK: - PROPERTIES -
+    
+    
+    // MARK: - ACTIONS -
+    
+    
+    // MARK: - METHODS -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
