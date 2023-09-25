fe67310f236d062ca26b94006b2a35bb212d54a0
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-09-25 13:25:32 +0500

    - Handled back image with if-let condition

--- Sources/SmilesManCity/ViewControllers/ManCityHome/ManCityHomeViewController.swift
+++ Sources/SmilesManCity/ViewControllers/ManCityHome/ManCityHomeViewController.swift
@@ -199,7 +199,11 @@ public class ManCityHomeViewController: UIViewController {
         
         let btnBack: UIButton = UIButton(type: .custom)
         btnBack.backgroundColor = isLightContent ? .white : UIColor(red: 226.0 / 255.0, green: 226.0 / 255.0, blue: 226.0 / 255.0, alpha: 1.0)
-        btnBack.setImage(UIImage(named: AppCommonMethods.languageIsArabic() ? "back_icon_ar" : "back_icon", in: .module, compatibleWith: .current), for: .normal)
+        if let backImage = UIImage(named: AppCommonMethods.languageIsArabic() ? "back_icon_ar" : "back_icon", in: .module, compatibleWith: .current) {
+            btnBack.setImage(backImage, for: .normal)
+        } else {
+            btnBack.setImage(UIImage(systemName: "return"), for: .normal)
+        }
         btnBack.addTarget(self, action: #selector(self.onClickBack), for: .touchUpInside)
         btnBack.frame = CGRect(x: 0, y: 0, width: 32, height: 32)
         btnBack.layer.cornerRadius = btnBack.frame.height / 2
